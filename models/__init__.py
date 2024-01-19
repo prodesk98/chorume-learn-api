7728d4252bcb09f16c3923d06796c413138fc7e7
prodesk98 <133387879+prodesk98@users.noreply.github.com>
2024-01-19 01:42:23 -0300

    api

--- models/__init__.py
+++ models/__init__.py
@@ -2,7 +2,8 @@ from .api import (
     UpsertResponse, QueryResponse, UpsertRequest,
     AnswerResponse, AnswerRequest, TextToVoiceRequest,
     TextToVoiceResponse, MillionShowRequest, MillionShowResponse,
-    VectorFilterRequest, VectorDeleteRequest
+    VectorFilterRequest, VectorDeleteRequest, VectorDeleteResponse,
+    VectorUsernamesDeleteRequest, VectorUsernamesDeleteResponse
 )
 from .documents import Document
 from .tasks import UpsertTasksDocument, DocumentTasksSearch
